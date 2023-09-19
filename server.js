5bdba3901b0a1f5309af5417153c59b7552fb219
nfluckiger20 <flu20002@byui.edu>
2023-09-19 12:39:10 -0600

    working

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const index_1 = __importDefault(require("./routes/index"));
-const app = (0, express_1.default)();
-const port = Number(process.env.PORT) || 8000;
-app.use('/', index_1.default);
-app.listen(port, () => (console.log('server running on port ${port}')));
+const express = require('express');
+const app = express();
+
+const port =(process.env.PORT) || 8000;
+
+app.use("/", require("./routes"));
+
+app.listen(port, () => (
+    console.log('server running on port ${port}')
+))
