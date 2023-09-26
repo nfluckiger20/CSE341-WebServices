e44f77498131f96dff9b6cdef389d7aed2528b95
nfluckiger20 <flu20002@byui.edu>
2023-09-26 14:15:19 -0600

    Token

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ const port =(process.env.PORT) || 8000;
 app.use("/", require("./routes"));
 
 app.listen(port, () => (
-    console.log('server running on port ${port}')
+    console.log(`server running on port ${port}`)
 ))
