dbb713d9c0f29df00c16a661379e9114a67df1c5
nfluckiger20 <flu20002@byui.edu>
2023-10-20 16:48:20 -0600

    l06 success

--- models/location.js
+++ models/location.js
@@ -27,23 +27,6 @@ module.exports = (mongoose) => {
       inventory_name: {
         type: String
       },
-      Locations: {
-        USA: {
-          States: [mongoose.SchemaTypes.Mixed]
-        },
-        Canada: {
-          Provinces: [mongoose.SchemaTypes.Mixed]
-        },
-        USA_outlets: {
-          type: [mongoose.SchemaTypes.Mixed]
-        },
-        Canada_outlets: {
-          type: [String]
-        },
-        Additional: {
-          None: [mongoose.SchemaTypes.Mixed]
-        }
-      }
     });
   
     return mongoose.model('location', locationSchema);
